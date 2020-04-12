b2d54c08874c5af41011ebae000f7cb9f8b78c61
vfan999 <844139601@qq.com>
2020-04-12 17:57:34 +0800

    first commit

---
+++ nfs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#自动挂载三号机nfs,SETBOOT参数0为不开机启动
+
+SETBOOT=0
+MOUNT=192.168.1.174:/root/shell
+
+boot="#!/bin/bash \n
+\n
+mount -t nfs $MOUNT /nfs \n
+#umount /nfs/
+"
+
+cd $(dirname $0)
+if ! mountpoint -q /nfs/ ;then
+	yum list installed | grep nfs-utils || yum install -y nfs-utils
+	[[ -d /nfs ]] || mkdir /nfs
+	if mount -t nfs $MOUNT /nfs ;then
+		echo '/nfs/ mount'
+	else 
+		echo 'nfs服务器未启动'
+		exit 1
+	fi
+else
+	umount /nfs/
+	echo '/nfs/ already umount'
+fi
+
+[[ $SETBOOT == 0 ]] && exit 0
+chmod +x /etc/rc.d/rc.local
+grep "^sh ~/boot.sh" /etc/rc.local || echo 'sh ~/boot.sh' >> /etc/rc.local
+if [[ -f ~/boot.sh ]];then
+	grep "mount -t nfs $MOUNT /nfs" ~/boot.sh || echo "mount -t nfs $MOUNT /nfs" >> ~/boot.sh
+else
+	echo -e $boot > ~/boot.sh
+fi
